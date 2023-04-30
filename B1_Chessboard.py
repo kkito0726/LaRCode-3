ef12d45939cd94ab817bae3aafe5bba6dca40a7b
RedFieldf <momosuzy0216@gmail.com>
2023-04-30 10:07:32 +0900

    初めてのコミットです

--- B1_Chessboard.py
+++ B1_Chessboard.py
@@ -3,3 +3,21 @@ AtCoder Beginner Contest 296
 https://atcoder.jp/contests/abc296/tasks/abc296_b
 B問題
 '''
+
+# Sを入力
+S = [input() for _ in range(8)]
+
+# アルファベットa~hを並べた「line」変数を設定
+line = ["a","b","c","d","e","f","g","h"]
+
+# for文で8*8のマスにおける"*"の有無を探索
+for i in range(8):
+    for j in range(8):
+
+        # "*"のあるマスのx座標とy座標の値を指定  
+        if S[i][j] == "*":
+            ans_y = str(8-i)
+            ans_x = str(line[j])
+
+# 答えをprint
+print(ans_x + ans_y)
