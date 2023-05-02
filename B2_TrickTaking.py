0c9ffd38395e156b9f72883be6973dc4151bddbd
S.Shiomi <shunsukeshiomi0914@gmail.com>
2023-05-03 00:38:53 +0900

    回答完了

--- B2_TrickTaking.py
+++ B2_TrickTaking.py
@@ -3,3 +3,25 @@
 https://atcoder.jp/contests/abc299/tasks/abc299_b
 B問題
 '''
+N, T = map(int, input().split())
+C, R = [list(map(int, input().split())) for i in range(2)]
+
+list=[]
+
+if T in C:
+    for i,s in enumerate(C):
+        if s == T:
+            list.append(R[i])
+        else:
+            list.append(0)
+else:
+    color = C[0] 
+    for i,s in enumerate(C):
+        if s == color:
+            list.append(R[i])
+        else:
+            list.append(0)
+
+print(list.index(max(list))+1)
+
+
