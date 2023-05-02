41c95c84a35b8317d814006e7b4ada424d2599a6
S.Shiomi <shunsukeshiomi0914@gmail.com>
2023-05-02 17:40:57 +0900

    回答完了

--- B1_Chessboard.py
+++ B1_Chessboard.py
@@ -3,3 +3,17 @@ AtCoder Beginner Contest 296
 https://atcoder.jp/contests/abc296/tasks/abc296_b
 B問題
 '''
+#パターン１
+
+N = 8
+komoji = [chr(i) for i in range(ord('a'),ord('h')+1)]
+
+list = []
+for i in range(N):
+   list.append(input())
+
+for i, s  in enumerate(list):
+   if "*" in s:
+      x = str(komoji[s.index("*")])
+      y = str(N - i)
+      print(x+y)
