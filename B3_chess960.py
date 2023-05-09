58ffda22d37219cc4003cd344fda362681ecf702
RedFieldf <momosuzy0216@gmail.com>
2023-05-09 16:36:04 +0900

    修正済、全テストコードクリアしたものです

--- B3_chess960.py
+++ B3_chess960.py
@@ -7,24 +7,46 @@ B問題
 # 入力
 S = list(input())
 
-# ans関数を設定
-ans = "No"
 
-# 2つのBのindexが、奇偶異なるものを抽出
-for a in range(len(S)):
-    for b in range(a+1, len(S)):
-        if S[a] == 'B' and S[b] == 'B':
-            if a % 2 != b % 2:
+# 条件1のクリアをみるflag1と、条件2のクリアをみるfrag2を設定
+flag1 = False
+flag2 = False
+
+
+# 空リストを作成
+B_index = []
+
+# そのリストにBのindexのみ追加
+for i in range(len(S)):
+    if S[i] == 'B':
+        B_index.append(i+1)
+
+# Bのindexの奇偶が異なる場合、flag1をTrueに変更
+for _ in B_index:
+    if B_index[0] % 2 != B_index[1] % 2:
+        flag1 = True
                 
-                # x < z < yの関係を指定
-                for x in range(len(S)):
-                    for z in range(x+1, len(S)):
-                        for y in range(z+1, len(S)):
-                        
-                            # 2つのRのindexが、奇偶異なるものを抽出
-                            if S[x] == 'R' and S[y] == 'R' and S[z] == 'K':
-                                if x % 2 != y % 2:
-                                    ans = "Yes"
+
+# 空リストを作成
+K_index = []
+R_index = []
+
+# そのリストにKとRのindexのみそれぞれ追加
+for i in range(len(S)):
+    if S[i] == "K":
+        K_index.append(i+1)
+    
+    if S[i] == "R":
+        R_index.append(i+1)
+
+# KがRの間にある場合、flag2をTrueに変更
+for _ in K_index:
+    if R_index[0]  < K_index[0] and K_index[0] < R_index[1]:
+        flag2 = True
 
 # 出力
-print(ans)
+if flag1 and flag2:
+    print("Yes")
+
+else:
+    print("No")
