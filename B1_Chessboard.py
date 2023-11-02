b2a17cf929c4c0d7caee7ded5e199776c3b13146
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2023-11-02 16:47:06 +0900

    B1問題解答完了

--- B1_Chessboard.py
+++ B1_Chessboard.py
@@ -3,3 +3,13 @@ AtCoder Beginner Contest 296
 https://atcoder.jp/contests/abc296/tasks/abc296_b
 B問題
 '''
+str_list = [input() for _ in range(8)]
+
+for i in range(len(str_list)):
+    for j in range(len(str_list[i])):
+        num2alpha = lambda c: chr(65+j)
+        if str_list[i][j] == '*':
+            print((num2alpha(j).lower()), end="")
+            print(8-i)
+        else:
+            pass
