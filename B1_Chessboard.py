0c316b6b3a9a7f7013ba0b1057a90e967e7c99d1
takaaki <takaaki0210123@gmail.com>
2023-11-08 16:42:44 +0900

    回答完了しました

--- B1_Chessboard.py
+++ B1_Chessboard.py
@@ -3,3 +3,15 @@ AtCoder Beginner Contest 296
 https://atcoder.jp/contests/abc296/tasks/abc296_b
 B問題
 '''
+#入力
+S = [input() for _ in range(8)]
+#出力
+H = ['a','b','c','d','e','f','g','h']
+J = ['8','7','6','5','4','3','2','1']
+for i in range(8):
+    for n in range(8):
+        if S[i][n] == '*':
+            print(H[n]+J[i])
+
+            
+
