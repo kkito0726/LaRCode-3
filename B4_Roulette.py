49fe077eb489236c8f2b51bab99272dd17cd6129
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2023-11-24 14:11:28 +0900

    説明を追加しました。

--- B4_Roulette.py
+++ B4_Roulette.py
@@ -15,12 +15,16 @@ X = int(input())
 #出力
 num_buy = 37
 B = []
+#N人分の賭け数を調べていく
 for i in range(N):
+    #i番目の人がXにかけていた時に以下の処理を実行
     if X in A_list[i]:
+        #最大37点賭けられる中で点数が少ない人が現れたらnum_buyを更新し、以前のBをリセットし、i+1番目の人をBに加える。
         if C_list[i] < num_buy:
             num_buy = C_list[i]
             B.clear()
             B.append(i+1)
+        #最小点数と同じ購入数であればi+1番目の人をBに加える。
         elif C_list[i] == num_buy:
             B.append(i+1)
 print(len(B))
