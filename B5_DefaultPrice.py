ade98aa57041b267046475c753aaed772d3c233b
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2023-11-24 14:18:25 +0900

    説明を追加しました。

--- B5_DefaultPrice.py
+++ B5_DefaultPrice.py
@@ -11,9 +11,12 @@ price = list(map(int, input().split()))
 #出力
 pay = 0
 for i in range(N):
+    #値段が記載されているお皿の色の中にi番目に食べたお皿の色が含まれているか確認する。
     if eat_color[i] in dish_color:
+        #食べたお皿の色と同じ色がdish_colorのどの位置にあるか特定して値段をpayに加える。
         pay += price[dish_color.index(eat_color[i]) + 1]
     else:
+        #食べたお皿の色の値段が決まっていないとき、priceの0番目をpayに加える。
         pay += price[0]
 print(pay)
     
