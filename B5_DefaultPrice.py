b7fcf969d5e4594e396281a1bb62ecb217c99caf
RedFieldf <momosuzy0216@gmail.com>
2023-11-27 17:06:27 +0900

    add new file

--- B5_DefaultPrice.py
+++ B5_DefaultPrice.py
@@ -3,3 +3,19 @@ CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
 URL: https://atcoder.jp/contests/abc308/tasks/abc308_b
 B問題
 '''
+
+N, M = map(int,input().split())
+
+C = list((input().split()))
+D = list((input().split()))
+P = list(map(int, input().split()))
+
+total = 0
+
+for i in range(N):
+    if C[i] in D:
+        total += P[D.index(C[i]) + 1]
+    else:
+        total += P[0]
+
+print(total)
