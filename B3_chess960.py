e970a990abdc3dace275960cce3c51627ad0ffd6
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-11-28 23:19:17 +0900

    B3解答完了

--- B3_chess960.py
+++ B3_chess960.py
@@ -3,3 +3,16 @@ AtCoder Beginner Contest 297
 https://atcoder.jp/contests/abc297/tasks/abc297_b
 B問題
 '''
+S = list(input())
+score = 0
+R = []
+K = S.index('K')
+for i in range(8):
+    if S[i] == 'R':
+        R.append(i)
+    if S[i] == 'B' and i % 2 == 0:
+        score += 1
+if score == 1 and R[0] < K < R[1]:
+    print("Yes")
+else:
+    print("No")
