4838a5e453d9c3fe6da34fca8bbc10de02a628b7
takaaki <takaaki0210123@gmail.com>
2023-11-28 12:54:14 +0900

    こっちはまだ未完了のパターンです

---
+++ B4_Roulette_pattern1.py
@@ -0,0 +1,48 @@
+'''
+AtCoder Beginner Contest 314
+URL: https://atcoder.jp/contests/abc314/tasks/abc314_b
+B問題
+'''
+#入力
+N = int(input())
+C = []
+A = []
+for i in range(N):
+    C.append(int(input()))
+    A.append(list(map(int,input().split())))
+X = int(input())
+#print(N)
+#print(C)
+#print(A)
+#print(X)
+
+#出力1
+M = []
+victory = []
+#X(当たり)があるか見てる
+for i in range(N):
+    if (X in A[i]) == True:
+        M.append(C[i])
+        
+#当たった人の中でかけた数が少ない人をvictoryリストに入れてる
+if len(M) > 0:
+    for n in range(len(M)):
+        if M[n] == min(M):
+            victory.append(C.index(M[n])+1)
+            C[C.index(M[n])] = N
+    print(M.count(min(M)))
+#リストを文字列にしてます。
+    print(' '.join(map(str,victory)) ) 
+else :
+    M.append(0)
+    print(' '.join(map(str,M)) ) 
+#リストを文字列にしてます。        
+#出力2
+for k in range(N):
+    if (X in A[k]) == False:
+        C[k] = None
+print(C)
+print(min(C))
+#for n in range(N):
+#    if (X in A[i]) == True and C[i] = min(C):
+        
