2a3466c2e8102ddbd430b9f849d42a4ab61015a7
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-11-28 17:40:03 +0900

    B1解答完了

--- B1_Chessboard.py
+++ B1_Chessboard.py
@@ -3,3 +3,9 @@ AtCoder Beginner Contest 296
 https://atcoder.jp/contests/abc296/tasks/abc296_b
 B問題
 '''
+S = [list(input()) for _ in range(8)]
+Si = [8, 7, 6, 5, 4, 3, 2, 1]
+Sj = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+for i in range(8):
+    if '*' in S[i]:
+        print(Sj[S[i].index('*')] + str(Si[i]))
