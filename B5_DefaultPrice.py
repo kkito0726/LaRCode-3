f3012f3a1717e5fe576247cd4db6225152bc6ebc
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-11-30 22:32:22 +0900

    B5解答完了

--- B5_DefaultPrice.py
+++ B5_DefaultPrice.py
@@ -3,3 +3,16 @@ CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
 URL: https://atcoder.jp/contests/abc308/tasks/abc308_b
 B問題
 '''
+N, M = map(int, input().split())
+C = list(input().split())
+D = list(input().split())
+P = list(map(int, input().split()))
+result = []
+ans = 0
+for i in range(N):
+    if C[i] in D:
+        result.append(D.index(C[i])+1)
+    elif C[i] not in D:
+            result.append(0)
+    ans += P[result[i]]
+print(ans)
