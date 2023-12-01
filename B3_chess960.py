8d1fc981c7a4df5dd867d5382857a8480bf761cb
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-12-01 13:48:07 +0900

    B3コメント追加

--- B3_chess960.py
+++ B3_chess960.py
@@ -5,13 +5,19 @@ B問題
 '''
 S = list(input())
 score = 0
+#Sの中のRの位置のリスト
 R = []
+#Sの中のKの位置
 K = S.index('K')
 for i in range(8):
+    #Sの中のRの位置をリストRに入れる
     if S[i] == 'R':
         R.append(i)
+    #Bが偶数ならscoreに+1
     if S[i] == 'B' and i % 2 == 0:
         score += 1
+#Bが偶奇かどうか、偶2ならscore=2、偶奇ならscore=1、奇2ならscore=0
+#リストRの0番目よりもKが大きく、リストRの1番目よりもKが小さいならKは間に挟まれている
 if score == 1 and R[0] < K < R[1]:
     print("Yes")
 else:
