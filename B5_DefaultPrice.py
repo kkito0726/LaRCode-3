520540f153c38d614ed98d69015d61bea392bf84
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-12-01 13:47:11 +0900

    B5解答完了

--- B5_DefaultPrice.py
+++ B5_DefaultPrice.py
@@ -7,12 +7,12 @@ N, M = map(int, input().split())
 C = list(input().split())
 D = list(input().split())
 P = list(map(int, input().split()))
-list = []
+result = []
 ans = 0
 for i in range(N):
     if C[i] in D:
-        list.append(D.index(C[i])+1)
+        result.append(D.index(C[i])+1)
     elif C[i] not in D:
-            list.append(0)
-    ans += P[list[i]]
+            result.append(0)
+    ans += P[result[i]]
 print(ans)
