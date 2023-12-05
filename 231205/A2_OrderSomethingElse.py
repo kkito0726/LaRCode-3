b1e718701746ab8dfac4770f78fbfc4f27f16cae
moeno <moeno.furuya.5n@stu.hosei.ac.jp>
2023-12-05 19:16:08 +0900

    A2解答完了

--- 231205/A2_OrderSomethingElse.py
+++ 231205/A2_OrderSomethingElse.py
@@ -3,3 +3,22 @@ freee プログラミングコンテスト2023（AtCoder Beginner Contest 310）
 URL: https://atcoder.jp/contests/abc310/tasks/abc310_a
 A問題
 """
+
+N,P,Q = map(int, input().split())
+nedan = list(map(int, input().split()))
+
+# print(N,P,Q)
+# print(nedan)
+
+Min = min(nedan)
+
+total = Q
+total += Min
+
+if P > total:
+    print(total)
+else:
+    print(P)
+
+
+
