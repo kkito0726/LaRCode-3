7f5eea25539de984faf379985391e9ff34764939
moeno <moeno.furuya.5n@stu.hosei.ac.jp>
2023-12-05 19:14:33 +0900

    A1解答完了

--- 231205/A1_flip.py
+++ 231205/A1_flip.py
@@ -2,3 +2,8 @@
 Ｓｋｙ株式会社プログラミングコンテスト2023（AtCoder Beginner Contest 289）
 URL: https://atcoder.jp/contests/abc289/tasks/abc289_a
 """
+
+s = input()
+
+print(s.replace('0','x').replace('1','0').replace('x','1'))
+
