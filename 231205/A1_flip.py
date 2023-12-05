fbb6198e847cfb105f0876528df9df39db9a4190
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-12-05 19:13:44 +0900

    A1解答完了

--- 231205/A1_flip.py
+++ 231205/A1_flip.py
@@ -2,3 +2,11 @@
 Ｓｋｙ株式会社プログラミングコンテスト2023（AtCoder Beginner Contest 289）
 URL: https://atcoder.jp/contests/abc289/tasks/abc289_a
 """
+S = input()
+ans = ""
+for i in S:
+    if i == "0":
+        ans += "1"
+    else:
+        ans += "0"
+print(ans)
