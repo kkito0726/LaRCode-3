826fd51ed877d78d6389cf178e017a683a22716a
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-12-05 19:15:27 +0900

    B1解答完了

--- 231205/B1_MissingNo.py
+++ 231205/B1_MissingNo.py
@@ -3,3 +3,11 @@
 URL: https://atcoder.jp/contests/abc317/tasks/abc317_b
 B問題
 """
+N = int(input())
+A = list(map(int, input().split()))
+A.sort()
+ans = 0
+for i in range(N):
+    if 0 < i and A[i] - A[i-1] == 2:
+        ans += (A[i]-1)
+print(ans)
