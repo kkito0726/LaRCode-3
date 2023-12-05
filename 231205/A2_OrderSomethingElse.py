d09631e054aed997beb86eaabb2085179cc71f38
takaaki <takaaki0210123@gmail.com>
2023-12-05 19:12:50 +0900

    AC

--- 231205/A2_OrderSomethingElse.py
+++ 231205/A2_OrderSomethingElse.py
@@ -3,3 +3,14 @@ freee プログラミングコンテスト2023（AtCoder Beginner Contest 310）
 URL: https://atcoder.jp/contests/abc310/tasks/abc310_a
 A問題
 """
+#入力
+N,price,price2 = map(int,input().split())
+tuika = list(map(int,input().split()))
+
+#出力
+total = 0
+if min(tuika)+price2 <price:
+    total += min(tuika)+price2
+else :
+    total+=price
+print(total)
--- 231205/B1_MissingNo.py
+++ 231205/B1_MissingNo.py
@@ -3,3 +3,21 @@
 URL: https://atcoder.jp/contests/abc317/tasks/abc317_b
 B問題
 """
+#入力
+N = int(input())
+A = list(map(int, input().split()))
+
+#出力
+ans = sorted(A)
+ans2=[]
+for i in range(ans[0],ans[-1]+1):
+    ans2.append(i)
+for i in ans2:
+    if i not in ans:
+        print(i)
+
+
+#SUM = 
+
+#for i in A:
+#   SUM -= i
