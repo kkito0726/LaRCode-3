b73fd5272fa30c71e16e87680f8076df2eece96d
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-12-05 19:14:44 +0900

    A2解答完了

--- 231205/A2_OrderSomethingElse.py
+++ 231205/A2_OrderSomethingElse.py
@@ -3,3 +3,9 @@ freee プログラミングコンテスト2023（AtCoder Beginner Contest 310）
 URL: https://atcoder.jp/contests/abc310/tasks/abc310_a
 A問題
 """
+N, P, Q = map(int, input().split())
+D = list(map(int, input().split()))
+if (Q + min(D)) < P:
+    print(Q + min(D))
+else:
+    print(P)
