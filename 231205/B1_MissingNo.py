55fa3359342a1e411b49066b6cdfb9001c8843ba
takaaki <takaaki0210123@gmail.com>
2023-12-08 16:30:55 +0900

    complete

--- 231205/B1_MissingNo.py
+++ 231205/B1_MissingNo.py
@@ -10,12 +10,17 @@ A = list(map(int, input().split()))
 #出力
 ans = sorted(A)
 ans2=[]
+a =0
 for i in range(ans[0],ans[-1]+1):
     ans2.append(i)
+
 for i in ans2:
     if i not in ans:
         print(i)
-
+    else :
+        a +=1
+if a == N+1:
+    print(ans[-1]+1)
 
 #SUM = 
 
--- 231205/C01-TaxRate.py
+++ 231205/C01-TaxRate.py
@@ -2,17 +2,13 @@
 Ｓｋｙ株式会社プログラミングコンテスト2023（AtCoder Beginner Contest 289）
 URL: https://atcoder.jp/contests/abc289/tasks/abc289_a
 """
-#入力
-S = input()
-#出力
-ans =''
-for i in S:
-    if i == '0':
-        ans +='1'
-    else :
-        ans +='0'
-print(ans)
-        
+
+N = int(input())
+import math
+ans = N*1.1
+print(math.floor(ans))
+    
+
     
 
 
