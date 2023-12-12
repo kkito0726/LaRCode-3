1de91bcf8af03df235a59a9eb91e1817cacabc29
conne <kanki1359@icloud.com>
2023-12-12 15:52:03 +0900

    python大会A1

--- 231205/A1_flip.py
+++ 231205/A1_flip.py
@@ -2,3 +2,12 @@
 Ｓｋｙ株式会社プログラミングコンテスト2023（AtCoder Beginner Contest 289）
 URL: https://atcoder.jp/contests/abc289/tasks/abc289_a
 """
+s = input()  # sはstr型
+
+T = ""
+for i in s:
+    if i == "0":
+        T += "1"
+    elif i == "1":
+        T += "0"
+print(T)
