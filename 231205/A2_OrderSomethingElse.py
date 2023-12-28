79746274b456b8b1ab9fbeb03557cc0d55f8f8f3
conne <kanki1359@icloud.com>
2023-12-28 16:52:18 +0900

    1回目

--- 231205/A2_OrderSomethingElse.py
+++ 231205/A2_OrderSomethingElse.py
@@ -3,3 +3,17 @@ freee プログラミングコンテスト2023（AtCoder Beginner Contest 310）
 URL: https://atcoder.jp/contests/abc310/tasks/abc310_a
 A問題
 """
+a,b,c = map(int, input().split())  # 3個の数字の入力を受け取る                     # 出力を確認
+num_list = list(map(int, input().split()))  # n個の数字がリストに格納される
+
+price = 0
+for i in num_list :
+    if b <= c + min(num_list):
+        price = b
+    else :
+        price = c + min(num_list)
+
+print(price)
+
+
+
