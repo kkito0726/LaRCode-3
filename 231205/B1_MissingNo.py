14fbb789758a78638855106f1b660a4059a1205a
conne <kanki1359@icloud.com>
2023-12-29 17:51:23 +0900

    1回目

--- 231205/B1_MissingNo.py
+++ 231205/B1_MissingNo.py
@@ -3,3 +3,12 @@
 URL: https://atcoder.jp/contests/abc317/tasks/abc317_b
 B問題
 """
+n = int(input())  # nはint型
+num_list = list(map(int, input().split()))  # n個の数字がリストに格納される
+
+new_list = sorted(num_list)
+
+for i in range(n) :
+    if new_list[i+1] - new_list[i] != 1:
+         print(new_list[i]+1)
+         break
